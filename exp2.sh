bc637b1c50c282e5bd17aed6b644941bfe2b3b8f
Abhijit Mudigonda <abhijit-mudigonda@gmail.com>
2020-01-07 18:47:48 -0800

    gurobi install script added

--- exp2.sh
+++
@@ -1,5 +0,0 @@
-pip install pulp
-git clone https://github.com/abhijit-mudigonda/and-maj 
-cd and-maj
-python sscript.py
-
--- gurobi_install.sh
+++ gurobi_install.sh
@@ -1,5 +1,6 @@
 #Hi Yilun!
 
+ENTERKEYHERE=ifyouareseeingthismessageputyourgurobikeyhere
 wget https://packages.gurobi.com/9.0/gurobi9.0.0_linux64.tar.gz
 
 md5hash=$(md5sum gurobi9.0.0_linux64.tar.gz | awk '{print $1;}')
@@ -15,7 +16,7 @@ tar -xvzf gurobi9.0.0_linux64.tar.gz --exclude='gurobi900/linux64/docs/*' --excl
 printf 'export GUROBI_HOME="${HOME}/gurobi900/linux64"\nexport PATH="${PATH}:${GUROBI_HOME}/bin" \nexport LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"' >> .bashrc
 source .bashrc
 
-grbgetkey d3a69eaa-3070-11ea-8d94-0a7c4f30bdbe
+grbgetkey $ENTERKEYHERE
 
 cd ${HOME}/gurobi900/linux64
 sudo python setup.py install
@@ -23,7 +24,4 @@ cd ${HOME}
 
 echo "Installation of gurobi should be done."
 
-git clone https://github.com/abhijit-mudigonda/and-maj 
-cd and-maj
-python sscript.py -solver "gurobi"
 
